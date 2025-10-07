bbdd0e94b28ec2516be2c0a48bfe230f44247b22
Dibya kanti das <50736105+dkdas1212@users.noreply.github.com>
2025-10-07 21:17:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
+import recommendRoutes from './routes/recommendRoutes.js'
+import interactionRoutes from './routes/interactionRoutes.js'
+
 
 dotenv.config({path: './.env'})
 
@@ -23,6 +26,8 @@ app.use('/api/products',productRoutes)
 app.use('/api/users',userRoutes)
 app.use('/api/orders',orderRoutes)
 app.use('/api/upload', uploadRoutes)
+app.use('/api/recommend', recommendRoutes)
+app.use('/api/interactions', interactionRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
